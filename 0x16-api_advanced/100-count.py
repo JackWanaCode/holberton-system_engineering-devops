798fe2d7b36caba820bb671ad96487054753022e
JackWanaCode <kazucuong@yahoo.com>
2019-02-14 22:06:28 +0000

    first commit

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""function that queries the Reddit API and prints the titles of the
+first 10 hot posts listed for a given subreddit
+"""
+import requests
+from collections import Counter
+
+
+def count_words(subreddit, word_list, after=1, count=Counter()):
+    """ prints the titles of the first 10 hot posts listed"""
+    if after is 1:
+        r = requests.get('https://www.reddit.com/r/{}/hot.json'.
+                         format(subreddit),
+                         headers={'User-Agent': 'jack'},
+                         allow_redirects=False)
+    else:
+        r = requests.get('https://www.reddit.com/r/{}/hot.json'.
+                         format(subreddit),
+                         headers={'User-Agent': 'jack'},
+                         params={'after': after},
+                         allow_redirects=False)
+    try:
+        val = r.json()['data']['children']
+        for item in val:
+            count.update(item['data']['title'].split())
+        after = r.json()['data']['after']
+        if after is None:
+            dic = {}
+            for item in word_list:
+                dic[item] = count[item]
+            for x, y in sorted(dic.items(), key=lambda x: x[1], reverse=True):
+                print("{}: {}".format(x, y))
+            return
+        else:
+            count_words(subreddit, word_list, after, count)
+    except:
+        pass
